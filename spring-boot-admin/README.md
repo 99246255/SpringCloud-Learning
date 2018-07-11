6b6976543cca49bc9580f509152c03e0d81298df
99246255 <99246255@qq.com>
2018-07-11 15:14:36 +0800

    c

---
+++ spring-boot-admin/README.md
@@ -0,0 +1,4 @@
+Spring Boot应用的健康监控
+
+springbootadmin版本问题
+暂时只支持1.5.2和Spring Cloud Dalston版本不然启动报错，所以单独写
---
+++ spring-boot-admin/src/main/java/com/github/springbootadmin/SpringBootAdminApplication.java
@@ -0,0 +1,17 @@
+package com.github.springbootadmin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringBootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
